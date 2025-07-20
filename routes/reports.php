0c72d23a023f6910332c12bc1c1e79412c190769
abdelhade <abdelhadeeladawy@gmail.com>
2025-07-20 20:23:41 +0300

    abdelhade2 frst push

--- routes/reports.php
+++ routes/reports.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
 
 Route::get('/reports/accounts-tree', [ReportController::class, 'accountsTree'])->name('accounts.tree');
+Route::get('/reports/balance-sheet', [ReportController::class, 'balanceSheet'])->name('balance-sheet');
