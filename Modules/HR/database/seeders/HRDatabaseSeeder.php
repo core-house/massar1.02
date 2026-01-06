33d97168cd5c188e97de9bce931ae4cd901b4260
essam000 <essamelkhouly77@gmail.com>
2026-01-06 17:45:49 +0200

    fix migration seco

--- Modules/HR/database/seeders/HRDatabaseSeeder.php
+++ Modules/HR/database/seeders/HRDatabaseSeeder.php
@@ -13,22 +13,26 @@ class HRDatabaseSeeder extends Seeder
     {
         $this->call([
             HRPermissionsSeeder::class,
+            // Location Data (Dependencies for Employees)
+            CountrySeeder::class,
+            StateSeeder::class,
+            CitySeeder::class,
+            TownSeeder::class,
+            // Organizational Data
+            DepartmentSeeder::class,
+            EmployeesJobSeeder::class,
+            ShiftSeeder::class,
+            ContractTypeSeeder::class,
+            LeaveTypeSeeder::class,
+            // Employee Data
+            EmployeeSeeder::class,
+            // Data dependent on Employees
             AttendanceProcessingTestSeeder::class,
             AttendanceSeeder::class,
-            CitySeeder::class,
-            ContractTypeSeeder::class,
-            CountrySeeder::class,
-            CreateTestAttendanceData::class,
-            CvSeeder::class,
-            DepartmentSeeder::class,
-            EmployeeSeeder::class,
-            EmployeesJobSeeder::class,
             KpiSeeder::class,
-            LeaveTypeSeeder::class,
-            ShiftSeeder::class,
-            StateSeeder::class,
-            TownSeeder::class,
-            AttendanceTestDataSeeder::class,
+            CvSeeder::class,
+            // CreateTestAttendanceData::class,
+            // AttendanceTestDataSeeder::class,
         ]);
     }
 }
