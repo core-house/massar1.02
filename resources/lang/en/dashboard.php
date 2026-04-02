3940a7dd09b8959439b16a2a38693bc2906776b3
MohamedElagawy <mohamedelagawy49@gmail.com>
2026-04-02 19:04:17 +0200

    updates

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'subscription_warning_title' => 'Subscription Warning',
+    'subscription_warning_body' => 'Your subscription will expire in :days days on :date. Please renew to avoid service interruption.',
+];
